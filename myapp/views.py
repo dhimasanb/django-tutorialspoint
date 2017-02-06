68c22259c43a0921d2c523afb6bbeb78cf242cad
Dhimas Akbar N.B <dhimasakbarnoorbaihaque@gmail.com>
2017-02-06 08:47:06 +0700

    import datetime & tambah def hello

--- myapp/views.py
+++ myapp/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import datetime
 
 # Create your views here.
 def index(request):
    text = """<h1>welcome to my app !</h1>"""
    return HttpResponse(text)
+
+def hello(request):
+   today = datetime.datetime.now().date()
+   return render(request, "hello.html", {"today" : today})
